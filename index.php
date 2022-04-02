c8517712540e22fa14a5c982e83cf20fe9700a99
Collatz-Conjecture315 <81220220+Collatz-Conjecture315@users.noreply.github.com>
2022-04-02 18:42:58 +0530

    new

--- index.php
+++ index.php
@@ -1,62 +1,15 @@
-<?php
-$dbhost = 'remotemysql.com';
-$dbuser = 'BS1CwOyLI0';
-$dbpass = 'S9h6V3fRuz';
-$dbname = 'BS1CwOyLI0';
-$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-if(! $conn ) {
-   die('Could not connect: ' . mysqli_error());
-}
-echo 'Connected successfully';
-mysqli_close($conn);
-
-?>
-<!DOCTYPE html>
-
-<html lang = "en">
-
-<head>
-    <meta charset = "UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content = "width=device-width, initial-scale=1.0">
-
-    <title>We3_Web_Weavers</title>
-
-    <style>
-        body{
-            text-align: center;
-            font-size: 30px;
-        }
-
-        .log_label{
-            font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        }
-    </style>
-
-</head>
-
-<body>
-    
-    <h1 id = "log_label">Login</h1> 
-    <br>
-
-    <form action = "home_page.html" method = "get">
-        <label>First Name</label>
-        <input type = "text" name = "first_name"/>
-        <br><br>
-        <label>Last Name</label>
-        <input type = "text" name = "second_name"/>
-        <br><br>
-        <label>Password</label>
-        <input type="text" name="passwd" />
-        <br><br>
-        <input type="submit" value="Submit" />
-    </form>
-    
-
-    
-</body>
-
-
+<html>
+   <head>
+      <title>Login Form</title>
+   </head>
+   <body>
+      <form action="conn.php" method="get">
+         <label>Username: </label>
+         <input type="text" name="user" /><br />
+         <label>Password: </label>
+         <input type="password" name="pass" /><br />
+         <input type="submit" value="Submit" />
+         <input type="reset" value="Clear" />
+      </form>
+   </body>
 </html>
