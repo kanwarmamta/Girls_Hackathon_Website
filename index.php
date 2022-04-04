7129f4c3fdbe9c7fcd7bfa9a5b89f196a40126e4
Collatz-Conjecture315 <81220220+Collatz-Conjecture315@users.noreply.github.com>
2022-04-04 16:30:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,23 @@
       <title>Login Form</title>
    </head>
    <body>
-      <form action="list_page.php" method="get">
-         <label>Username: </label>
-         <input type="text" name="user" /><br />
+      <form action="login.php" method="post">
+         <h2>Login Here</h2>
+         <!--
+         <label>First Name: </label>
+         <input type="text" name="first_name" /><br />
+         <label>Last name: </label>
+         <input type="text" name="last_name" /><br />
          <label>Password: </label>
-         <input type="password" name="pass" /><br />
-         <input type="submit" value="Submit" />
-         <input type="reset" value="Clear" />
+         <input type="password" name="pass" /><br /> -->
+         <input type="submit" value="Login" />
+         
+
+         
       </form>
+      <h2> Register Here: </h2>
+         <form action = "registration.php">
+            <input type = "submit" value = "Register">
+         </form>
    </body>
 </html>
